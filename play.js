d723d431cc5fcff2bbdc5f0b9d5b560993cd4405
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-09-09 18:46:20 +0200

    Working with Objects, Properties & Methods

--- play.js
+++ play.js
@@ -1,29 +1,24 @@
-const name = "Martin";
-let age = 30;
-const hasHobbies = true;
+const person = {
+    name: "Martin", // This is a key value pair. here "name:" = key and "Martin" = value
+    age: 30,
+    height: 181,
+    // The scope of the greet function is the global scope outside the object. To avoid getting undefined use one of the other methods
+    greet: () => {
+        console.log(`Hi I am ${this.name}`);
+    },
+    hello: () => {
+        console.log(`Hi I am ${person.name}`);
+    },
+    old: function() {
+        console.log(`Hi I am ${this.age} years old`);
+    },
+    tall() {
+        console.log(`Hi I am ${this.height}cm tall`);
+    }
+};
 
-// name = "Mars"; // This creates an error because the variable is a const and thus cannot be changed.
-age = 50;
-
-// const summarizeUser = function (userName, userAge, userHasHobby) { // --> named function in constant
-const summarizeUser = (userName, userAge, userHasHobby) => { // --> Arrow function. The diffrence except for the syntax is how the 'this' keyword behaves.
-   return (
-       `Name is ${userName}, age is ${userAge}, and the user has hobbies: ${userHasHobby}`
-   );
-}
-
-const add = (a,b) => {
-    return a + b;
-}
-
-const subtract = (a,b) => a - b; // --> shortform arrow function possible only when function only has a return statement
-
-const addOne = a => a + a; // --> when arrow function only has one argument the paranthases can be omitted
-
-const addRandom = () => Math.floor(Math.random()*10); // --> when arrow function does not have any arguments we have to include the parentasies.
-
-console.log(summarizeUser(name, age, hasHobbies));
-console.log(add(1,2));
-console.log(subtract(2,1));
-console.log(addOne(1));
-console.log(addRandom());
+console.log(person);
+person.greet();
+person.hello();
+person.old();
+person.tall();
