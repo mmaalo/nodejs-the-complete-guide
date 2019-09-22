ac96ef3194079cc2d9b6f137c512f3f4af33a2ee
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-09-22 06:27:22 +0200

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -29,6 +29,23 @@ app.use('/', (req, res, next) => {
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>404, Page Not Found</title>
+        </head>
+        <body>
+            <h1>404, Page Not Found</h1>    
+        </body>
+        </html>
+    `);
+});
+
 // Start Server
 const server = http.createServer(app);
 server.listen(3000);
