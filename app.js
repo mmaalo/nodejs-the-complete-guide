e96fdd6f7abc882d20b0090b2ac3f5e6aa6ac0f5
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-10-05 03:28:44 +0200

    Working on the Layouth with Partials

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-    res.status(404).render('404', {docTitle: 'Page Not Found'});
+    res.status(404).render('404', {
+        docTitle: 'Page Not Found',
+        path: ''
+    });
 });
 
 // Start Server
