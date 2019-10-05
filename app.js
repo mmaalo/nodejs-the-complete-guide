92ab4f89e92fb2add800f139ad1248a5fc7471d7
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-10-06 01:14:06 +0200

    Finshed Assignment with check/errormsg if user already exists.

--- app.js
+++ app.js
@@ -7,48 +7,35 @@
     // npm imports
         const express = require('express');
         const helmet = require('helmet');
+        const expressHbs = require('express-handlebars');
 
     // local imports
         const rootDir = require('./util/rootDir');
-        const indexRoute = require('./routes/indexRoute');
+        const indexData = require('./routes/indexRoute');
         const usersRoute = require('./routes/usersRoute');
 
 // middleware
     const app = express();
     app.use(helmet());
 
+    app.engine('hbs', expressHbs({
+        extname: 'hbs',
+        defaultLayout: 'main-layout',
+        layoutsDir: 'views/layouts'
+    }));
+    app.set('view engine', 'hbs');
+    app.set('views', 'views');
+
     app.use(express.static(path.join(rootDir, 'public')));
 
 // routes
-    app.use(indexRoute);
+    app.use(indexData.router);
     app.use(usersRoute);
 
     app.use((req, res, next) => {
-        res.status(404).send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Page Not Found</title>
-            <link rel="stylesheet" href="/css/main.css">
-        </head>
-        <body>
-            <header class="main-header">
-                <nav class="main-header__nav">
-                    <ul class="main-header__item-list">
-                        <li class="main-header__item"><a href="/">Home</a></li>
-                        <li class="main-header__item"><a href="/users">Users</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <h1>Page Not Found</h1>
-            </main>
-        </body>
-        </html>>
-        `);
+        res.render('404', {
+            docTitle: 'Page Not Found'
+        });
     });
 
 // start server
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,53 +4,57 @@
 
     // npm imports
         const express = require('express');
-        const router = express.Router(); 
+        const router = express.Router();
         const bodyParser = require('body-parser');
         const urlendcoded = bodyParser.urlencoded({extended: true});
 
     // local imports
 
+// variables
+const users = [];
+const exists = [];
+
 // routes
 
 router.get('/', (req, res, next) => {
-    res.status(200).send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Home</title>
-        <link rel="stylesheet" href="/css/main.css">
-        <link rel="stylesheet" href="/css/index.css">
-    </head>
-    <body>
-        <header class="main-header">
-            <nav class="main-header__nav">
-                <ul class="main-header__item-list">
-                    <li class="main-header__item"><a href="/">Home</a></li>
-                    <li class="main-header__item"><a href="/users">Users</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>Welcome to this site!</h1>
-            <form class="index-form" action="/" method="POST">
-                <div class="form-control">
-                    <label for="userName">User Name</label>
-                    <input type="text" name="userName" id="userName">
-                </div>
-                <button type="submit">Log User</button>
-            </form>
-        </main>
-    </body>
-    </html>
-    `);
+    res.render('index', {
+        docTitle: 'Home',
+        indexCSS: true,
+        activeHome: true
+    });
 });
 
 router.post('/', urlendcoded, (req, res, next) => {
-    console.log(req.body);
+    let userExists = false;
+
+    if (exists.length > 0) {
+        for (let element of exists) {
+            exists.pop();
+        }
+    }
+
+    if (users.length > 0) {
+        for (let element of users) {
+            if (element.userName == req.body.userName) {
+                userExists = true;
+            }
+        }
+        if (userExists == false) {
+            users.push(req.body);
+            exists.push('false');
+        } else {
+            exists.push('true');
+        }
+
+    } else {
+        users.push(req.body)
+        exists.push('false')
+    }
     res.redirect('/users');
 })
 
-module.exports = router;
+module.exports = {
+    router,
+    users,
+    exists
+}
