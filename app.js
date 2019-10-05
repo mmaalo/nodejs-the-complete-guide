b398ac60a844febf3918e4193448648f3e611860
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-10-05 18:34:37 +0200

    Finished Assingnment with PUG

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
     // local imports
         const rootDir = require('./util/rootDir');
-        const indexRoute = require('./routes/indexRoute');
+        const indexData = require('./routes/indexRoute');
         const usersRoute = require('./routes/usersRoute');
 
 // middleware
@@ -19,36 +19,18 @@
 
     app.use(express.static(path.join(rootDir, 'public')));
 
+    app.set('view engine', 'pug');
+    app.set('views', 'views');
+
 // routes
-    app.use(indexRoute);
+    app.use(indexData.router);
     app.use(usersRoute);
 
     app.use((req, res, next) => {
-        res.status(404).send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Page Not Found</title>
-            <link rel="stylesheet" href="/css/main.css">
-        </head>
-        <body>
-            <header class="main-header">
-                <nav class="main-header__nav">
-                    <ul class="main-header__item-list">
-                        <li class="main-header__item"><a href="/">Home</a></li>
-                        <li class="main-header__item"><a href="/users">Users</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <h1>Page Not Found</h1>
-            </main>
-        </body>
-        </html>>
-        `);
+        res.render('404', {
+            docTitle: 'Page Not Found',
+            path: ''
+        });
     });
 
 // start server
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -10,47 +10,23 @@
 
     // local imports
 
+const users = [];
+
 // routes
 
 router.get('/', (req, res, next) => {
-    res.status(200).send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Home</title>
-        <link rel="stylesheet" href="/css/main.css">
-        <link rel="stylesheet" href="/css/index.css">
-    </head>
-    <body>
-        <header class="main-header">
-            <nav class="main-header__nav">
-                <ul class="main-header__item-list">
-                    <li class="main-header__item"><a href="/">Home</a></li>
-                    <li class="main-header__item"><a href="/users">Users</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>Welcome to this site!</h1>
-            <form class="index-form" action="/" method="POST">
-                <div class="form-control">
-                    <label for="userName">User Name</label>
-                    <input type="text" name="userName" id="userName">
-                </div>
-                <button type="submit">Log User</button>
-            </form>
-        </main>
-    </body>
-    </html>
-    `);
+    res.render('index', {
+        docTitle: 'Home',
+        path: '/'
+    });
 });
 
 router.post('/', urlendcoded, (req, res, next) => {
-    console.log(req.body);
+    users.push(req.body);
     res.redirect('/users');
 })
 
-module.exports = router;
+module.exports = {
+    router: router,
+    users: users
+}
