087db2efc24e2b065854f2f6e72e17b591652f24
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-10-05 04:42:39 +0200

    Finsished Base Assignment Structure

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-// Module Imports
+// imports
 
-    // Node.js Modules
-    const http = require('http');
-    const path = require('path');
+    // core imports
+        const http = require('http');
+        const path = require('path');
+    
+    // npm imports
+        const express = require('express');
+        const helmet = require('helmet');
 
-    // NPM modulesthe
-    const express = require('express');
-    const helmet = require('helmet');
-    const expressHandlebars = require('express-handlebars')
+    // local imports
+        const rootDir = require('./util/rootDir');
+        const indexRoute = require('./routes/indexRoute');
+        const usersRoute = require('./routes/usersRoute');
 
-    // Local modules
-    const rootDir = require('./util/path');
-    const adminData = require('./routes/admin');
-    const shopRoutes = require('./routes/shop');
-
-// Main App Middleware
+// middleware
     const app = express();
     app.use(helmet());
 
-    // Set Templating engine and template folder
-    app.set('view engine','ejs');
-    app.set('veiws', 'views');
-
-    // Set static folder
     app.use(express.static(path.join(rootDir, 'public')));
 
-// Routes Middleware
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+// routes
+    app.use(indexRoute);
+    app.use(usersRoute);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-    res.status(404).render('404', {
-        docTitle: 'Page Not Found',
-        path: ''
+    app.use((req, res, next) => {
+        res.status(404).send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Page Not Found</title>
+            <link rel="stylesheet" href="/css/main.css">
+        </head>
+        <body>
+            <header class="main-header">
+                <nav class="main-header__nav">
+                    <ul class="main-header__item-list">
+                        <li class="main-header__item"><a href="/">Home</a></li>
+                        <li class="main-header__item"><a href="/users">Users</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>Page Not Found</h1>
+            </main>
+        </body>
+        </html>>
+        `);
     });
-});
 
-// Start Server
-const server = http.createServer(app);
-server.listen(3000);
+// start server
+    const server = http.createServer(app);
+    server.listen(3000);
