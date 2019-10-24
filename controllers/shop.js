494f48b5663d7144be0342caee99715e09591cf1
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-10-24 07:52:42 +0200

    Adding Another Item

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,9 +34,16 @@
         });
     }
 
+    exports.getOrders = (req, res, next) => {
+        res.render('shop/orders', {
+            docTitle: 'Orders',
+            path: '/orders'
+        });
+    }
+
     exports.getCheckout = (req, res, next) => {
         res.render('shop/checkout', {
             docTitle: 'Checkout',
             path: '/checkout'
-        })
+        });
     }
