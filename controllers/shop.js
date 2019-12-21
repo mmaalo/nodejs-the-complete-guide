bd29e26a43361f3abeff86846d310d075691a56d
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-12-21 19:29:31 +0100

    Fetching Products - Time to Practice

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,7 +24,7 @@
 
     exports.getProducts = (req, res, next) => {
         Product.fetchAll()
-        .then(([rows, fieldData]) => {
+        .then(([rows]) => {
             res.render('shop/product-list', {
                 products: rows,
                 docTitle: 'All Products',
