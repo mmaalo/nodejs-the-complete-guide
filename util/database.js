d560db0a34d8ed35c6ba732b1525573890e49edf
mmaalo <25216294+mmaalo@users.noreply.github.com>
2019-12-25 02:21:18 +0100

    Connecting to the Database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'password'
+// MYSQL - Workbench
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'password'
+// });
+
+// module.exports = pool.promise();
+
+
+// Sequelize:
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'password', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
