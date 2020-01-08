7c1278303e11bb3028689101010f6ecee6d956cc
mmaalo <25216294+mmaalo@users.noreply.github.com>
2020-01-08 18:25:07 +0100

    Resetting the Cart & Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -128,32 +128,41 @@
     }
 
     exports.postOrder = (req, res, next) => {
-       req.user.getCart()
-       .then(cart => {
-           return cart.getProducts();
-       })
-       .then(products => {
-           req.user.createOrder()
-           .then(order => {
-               return order.addProducts(products.map(product => {
-                   product.orderItem = { quantity: product.cartItem.quantity };
-                   return product
-               }));
-           })
-           .catch(err => console.log(err));
-           console.log(products);
-       })
-       .then(result => {
-           res.redirect('/orders');
-       })
-       .catch(err => console.log(err)); 
+        let fetchedCart = cart;
+        req.user.getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            req.user.createOrder()
+            .then(order => {
+                return order.addProducts(products.map(product => {
+                    product.orderItem = { quantity: product.cartItem.quantity };
+                    return product
+                }));
+            })
+            .catch(err => console.log(err));
+            console.log(products);
+        })
+        .then(result => {
+            fetchedCart.setProducts(null);
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err)); 
     }
 
     exports.getOrders = (req, res, next) => {
-        res.render('shop/orders', {
-            docTitle: 'Orders',
-            path: '/orders'
-        });
+        req.user.getOrders({include: ['products']})
+        .then(orders => {
+            res.render('shop/orders', {
+                docTitle: 'Orders',
+                path: '/orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
     }
 
     exports.getCheckout = (req, res, next) => {
